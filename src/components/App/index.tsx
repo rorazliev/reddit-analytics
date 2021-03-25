deaebbbd34002c18f71f18e2ff14146ddacafc9e
Ruslan Orazliev <rorazliev@outlook.com>
2021-03-24 19:17:42 -0500

    Register routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,8 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement, Suspense, useEffect } from 'react';
+import {
+  HashRouter as Router, Switch, Redirect, Route,
+} from 'react-router-dom';
+import style from './index.module.scss';
 import ignoreRejection from '../../helpers/ignoreRejection';
 
 const App: React.FC = (): ReactElement => {
@@ -11,7 +15,19 @@ const App: React.FC = (): ReactElement => {
   });
 
   // Render
-  return <div />;
+  return (
+    <Router>
+      <main className={style.main} role="main">
+        <Suspense fallback={null}>
+          <Switch>
+            <Route exact path="/" render={() => null} />
+            <Route exact path="/:subreddit" render={() => null} />
+            <Route path="*" render={() => <Redirect to="/" />} />
+          </Switch>
+        </Suspense>
+      </main>
+    </Router>
+  );
 };
 
 export default App as React.FC;
