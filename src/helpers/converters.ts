fa3950e6bf219ffd0790667c8c5482777eaa784f
Ruslan Orazliev <rorazliev@outlook.com>
2021-04-08 21:36:15 -0500

    Add converters

---
+++ src/helpers/converters.ts
@@ -0,0 +1,25 @@
+/**
+ * Hash weekday and hour to generate an id
+ * for a heat table cell
+ * @param {number} weekday
+ * @param {number} hour
+ * @returns {number}
+ */
+export const hash = (
+  weekday: number,
+  hour: number,
+): number => weekday * 100 + hour;
+
+/**
+ * Unhash an id to get weekday
+ * @param {number} hash
+ * @returns {number}
+ */
+export const unhashWeekday = (hash: number): number => Math.floor(hash / 100);
+
+/**
+ * Unhash an id to get hour
+ * @param {number} hash
+ * @returns {number}
+ */
+export const unhashHour = (hash: number): number => hash % 100;
